91a7be55a57cd1517228399df6ccde48e6ad8d49
Petr Lipatov <lipatov3@gmail.com>
2023-09-04 21:08:41 +0300

    feat: string UI and useForm hook

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import styles from "./string.module.css";
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { useFormInputs } from "../hooks/useForm";
+
 export const StringComponent: React.FC = () => {
+  const { handleChange, values } = useFormInputs();
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.inputContainer}>
+        <Input
+          name="string"
+          value={values.name}
+          onChange={handleChange}
+          maxLength={11}
+        />
+        <Button extraClass={styles.stringButton} text="Развернуть" />
+      </div>
     </SolutionLayout>
   );
 };
