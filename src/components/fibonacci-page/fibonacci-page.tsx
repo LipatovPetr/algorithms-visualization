c14bc0caeeac47dcc6538ffd0fe9fae3d03138a2
Petr Lipatov <lipatov3@gmail.com>
2023-09-06 23:41:05 +0300

    feat: fibanocci algo

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,18 +1,50 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import styles from "./fibonacci-page.module.css";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { MAX_VALUE } from "./constants";
 import { useFormInputs } from "../hooks/useForm";
+import { Circle } from "../ui/circle/circle";
+import { delay } from "../../utils";
+import { DELAY_IN_MS } from "../../constants/delays";
+
+const arr: number[] = [];
 
 export const FibonacciPage: React.FC = () => {
   const { handleChange, values } = useFormInputs();
+  const [fibArray, setFibArray] = useState<number[]>([]);
+
+  // useEffect(() => {
+  //   console.log(fibArray);
+  // }, [fibArray]);
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("Submit");
+    generateFibonacciSequence(Number(values.value));
   }
+
+  function calculateFibonacci(num: number): number {
+    if (num < 2) {
+      return num;
+    }
+
+    const fibValue = calculateFibonacci(num - 1) + calculateFibonacci(num - 2);
+
+    return fibValue;
+  }
+
+  const generateFibonacciSequence = async (length: number) => {
+    const sequence: number[] = [];
+
+    for (let i = 0; i <= length; i++) {
+      const fibValue = await calculateFibonacci(i);
+      sequence.push(fibValue);
+      console.log(sequence);
+      setFibArray([...sequence]);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  };
 
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
@@ -36,6 +68,12 @@ export const FibonacciPage: React.FC = () => {
           text="Рассчитать"
         />
       </form>
+      <div className={styles.circlesContainer}>
+        {fibArray &&
+          fibArray.map((el, index) => (
+            <Circle letter={`${el}`} index={index} key={index} />
+          ))}
+      </div>
     </SolutionLayout>
   );
 };
