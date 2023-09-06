c4fa9aad501c6f8931b952e2cc842b4cd80029ce
Petr Lipatov <lipatov3@gmail.com>
2023-09-06 18:45:40 +0300

    chores(layout): fibanocci ui added

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import styles from "./fibonacci-page.module.css";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { MAX_VALUE } from "./constants";
 
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form
+        className={styles.form}
+        onSubmit={() => {
+          console.log("hadleSubmit");
+        }}
+      >
+        <div className={styles.inputContainer}>
+          <Input
+            name="string"
+            // value={values.string || ""}
+            // onChange={handleChange}
+            // maxLength={MAX_LENGTH}
+          />
+          <p
+            className={styles.caption}
+          >{`Максимальное число — ${MAX_VALUE}`}</p>
+        </div>
+        <Button
+          type="submit"
+          extraClass={styles.submitButton}
+          text="Рассчитать"
+        />
+      </form>
     </SolutionLayout>
   );
 };
