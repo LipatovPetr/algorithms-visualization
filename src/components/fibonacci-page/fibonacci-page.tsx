821b0271d2ab828ba4d0c3be104d6c13b11ff5c9
Petr Lipatov <lipatov3@gmail.com>
2023-09-06 19:04:03 +0300

    feat: input validation in fib algo

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import styles from "./fibonacci-page.module.css";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { MAX_VALUE } from "./constants";
+import { useFormInputs } from "../hooks/useForm";
 
 export const FibonacciPage: React.FC = () => {
+  const { handleChange, values } = useFormInputs();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("Submit");
+  }
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-      <form
-        className={styles.form}
-        onSubmit={() => {
-          console.log("hadleSubmit");
-        }}
-      >
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.inputContainer}>
           <Input
-            name="string"
-            // value={values.string || ""}
-            // onChange={handleChange}
-            // maxLength={MAX_LENGTH}
+            type="number"
+            name="value"
+            value={values.value || ""}
+            onChange={handleChange}
+            max={19}
+            min={0}
           />
           <p
             className={styles.caption}
