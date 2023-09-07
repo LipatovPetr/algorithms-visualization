9722986b178731690a61618b0709003fd171b73a
Petr Lipatov <lipatov3@gmail.com>
2023-09-07 19:12:31 +0300

    layout: sorting page ui

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import styles from "./sorting-page.module.css";
+import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Column } from "../ui/column/column";
+import { Button } from "../ui/button/button";
+import { Direction } from "../../types/direction";
 
 export const SortingPage: React.FC = () => {
   return (
     <SolutionLayout title="Сортировка массива">
+      <div className={styles.inputsContainer}>
+        <div className={styles.radioContainer}>
+          <RadioInput label={"Выбор"} />
+          <RadioInput label={"Пузырёк"} />
+        </div>
 
+        <div className={styles.filtersContainer}>
+          <Button text={"По возрастанию"} sorting={Direction.Descending} />
+          <Button text={"По убыванию"} sorting={Direction.Descending} />
+        </div>
+
+        <Button text={"Новый массив"} />
+      </div>
     </SolutionLayout>
   );
 };
