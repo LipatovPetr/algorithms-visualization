b5886c96c67fa3a6edd24e0fe57acdcf6b6d85aa
Petr Lipatov <lipatov3@gmail.com>
2023-09-07 21:28:27 +0300

    layout: queue ui page

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from "./queue-page.module.css";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const QueuePage: React.FC = () => {
   return (
     <SolutionLayout title="Очередь">
-
+      <div className={styles.inputsContainer}>
+        <div className={styles.innerContainer}>
+          <Input />
+          <Button text={"Добавить"} />
+          <Button text={"Удалить"} />
+        </div>
+        <Button text={"Очистить"} />
+      </div>
     </SolutionLayout>
   );
 };
