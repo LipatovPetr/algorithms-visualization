3c062df8ac4f4ca49155046accadbbdb0a865bd2
Petr Lipatov <lipatov3@gmail.com>
2023-09-13 12:57:17 +0300

    refactor(fib algo): caching implemented with closure

--- src/components/fibonacci-page/algorithm.ts
+++ src/components/fibonacci-page/algorithm.ts
@@ -2,21 +2,36 @@ import { delay } from "../../utils";
 import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 import { Dispatch, SetStateAction } from "react";
 
-const memo = new Map();
+// cache decorator
 
-function calculateFibonacci(num: number): number {
-  if (memo.has(num)) {
-    return memo.get(num);
-  }
+function cacheDecorator(func: (arg: number) => number) {
+  const cache = new Map();
 
+  return function (arg: number) {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    }
+
+    let result = func(arg);
+    cache.set(arg, result);
+    return result;
+  };
+}
+
+// calculateNthFibonacciValue - calculates n-th value if fibanocci sequence
+
+function calculateNthFibValue(num: number): number {
   if (num < 2) {
     return num;
   }
-
-  const fibValue = calculateFibonacci(num - 1) + calculateFibonacci(num - 2);
-  memo.set(num, fibValue);
+  const fibValue =
+    calculateNthFibValue(num - 1) + calculateNthFibValue(num - 2);
   return fibValue;
 }
+
+const calculateNthFibValueCached = cacheDecorator(calculateNthFibValue);
+
+// generateFibonacciSequence - creates a fibanocci sequence by using calculateFibonacci in a loop
 
 export async function generateFibonacciSequence(
   length: number,
@@ -25,7 +40,7 @@ export async function generateFibonacciSequence(
   const sequence: number[] = [];
 
   for (let i = 0; i <= length; i++) {
-    const fibValue = await calculateFibonacci(i);
+    const fibValue = await calculateNthFibValueCached(i);
     sequence.push(fibValue);
     stateSetter([...sequence]);
     await delay(SHORT_DELAY_IN_MS);
