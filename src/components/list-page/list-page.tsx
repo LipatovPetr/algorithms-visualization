d220d63792b4bbb8b904e974bd992187874a7247
Petr Lipatov <lipatov3@gmail.com>
2023-09-20 23:59:13 +0300

    styles(layout):linked list ui added

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./list-page.module.css";
 
 export const ListPage: React.FC = () => {
   return (
     <SolutionLayout title="Связный список">
-
+      <form className={styles.form}>
+        <div className={styles.upperRow}>
+          <div>
+            <Input extraClass={styles.input} />
+            <p className={styles.caption}>{`Максимум — ${4} символов`}</p>
+          </div>
+          <Button text="Добавить в head" linkedList="small" />
+          <Button text="Добавить в tail" linkedList="small" />
+          <Button text="Удалить из head" linkedList="small" />
+          <Button text="Удалить из tail" linkedList="small" />
+        </div>
+        <div className={styles.bottomRow}>
+          <Input extraClass={styles.input} />
+          <Button text="Добавить в head" linkedList="big" />
+          <Button text="Добавить в tail" linkedList="big" />
+        </div>
+      </form>
     </SolutionLayout>
   );
 };
