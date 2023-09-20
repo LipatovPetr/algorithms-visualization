b2135cd0f2e1c61f2842d8f4de6418822904b37d
Petr Lipatov <lipatov3@gmail.com>
2023-09-21 00:00:02 +0300

    feat(list): list class added

---
+++ src/components/list-page/linked-list.ts
@@ -0,0 +1,44 @@
+export class Node<T> {
+  value: T;
+  next: Node<T> | null;
+  constructor(value: T, next?: Node<T> | null) {
+    this.value = value;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+interface ILinkedList<T> {
+  append: (element: T) => void;
+  getSize: () => number;
+  print: () => void;
+}
+
+class LinkedList<T> implements ILinkedList<T> {
+  private head: Node<T> | null;
+  private size: number;
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  append(element: T) {
+    const node = new Node(element);
+    this.size++;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    let curr = this.head;
+    let res = "";
+    while (curr) {
+      res += `${curr.value} `;
+      curr = curr.next;
+    }
+    console.log(res);
+  }
+}
+
+const list = new LinkedList<number>();
