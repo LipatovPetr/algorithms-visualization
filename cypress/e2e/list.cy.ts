1d8cd7e75115a8d52f565750670e1051a3da6133
Petr Lipatov <lipatov3@gmail.com>
2023-10-21 21:49:19 +0300

    feat(tests): Link add & remove by index tests added

--- cypress/e2e/list.cy.ts
+++ cypress/e2e/list.cy.ts
@@ -45,7 +45,7 @@ describe("Linked-List Component", () => {
     cy.getByData("circle-element").first().should("not.contain", "14");
   });
 
-  it.only("Should remove an element from the Tail", () => {
+  it("Should remove an element from the Tail", () => {
     cy.getByData("text-input").first().type("14");
     cy.contains("Добавить в tail").click();
 
@@ -56,5 +56,35 @@ describe("Linked-List Component", () => {
 
     cy.get('[class*="small"]').last().should("have.text", "14");
     cy.getByData("circle-element").last().should("not.contain", "14");
+  });
+
+  it("Should add an element by the index", () => {
+    cy.getByData("text-input").first().type("14");
+    cy.getByData("text-input").last().type("1");
+
+    cy.contains("Добавить по индексу").click();
+
+    cy.get('[class*="small"]').last().should("have.text", "14");
+
+    cy.wait(1500);
+    cy.getByData("circle-element").eq(1).should("have.text", "14");
+  });
+
+  it.only("Should remove an element by the index", () => {
+    cy.getByData("text-input").first().type("14");
+    cy.getByData("text-input").last().type("1");
+
+    cy.contains("Добавить по индексу").click();
+
+    cy.get('[class*="small"]').last().should("have.text", "14");
+
+    cy.wait(1500);
+    cy.getByData("circle-element").eq(1).should("have.text", "14");
+
+    cy.contains("Удалить по индексу").click();
+
+    cy.wait(500);
+
+    cy.getByData("circle-element").eq(1).should("not.contain", "14");
   });
 });
